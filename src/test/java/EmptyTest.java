94f36e1ce663654fcbe3e568fec454e5215c2536
David Ginzberg <DavidGinzberg@users.noreply.github.com>
2016-06-13 10:45:01 -0400

    Changed comment in EmptyTest.java
    Added a little nudge to encourage students to write their own unit tests.

--- src/test/java/EmptyTest.java
+++ src/test/java/EmptyTest.java
@@ -1,5 +1,6 @@
 /**
- * You can safely delete this file; it is just here so that git picks up the test/java directory structure.
+ * You can safely delete this file and replace it with your unit tests;
+ * This file is just here so that git picks up the test/java directory structure.
  */
 public class EmptyTest {
 }
